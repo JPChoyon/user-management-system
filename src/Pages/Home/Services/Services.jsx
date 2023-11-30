d3bc05561aabbab13e525be72abc86dde439b357
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-11-30 16:16:57 +0600

    final push

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,17 +1,26 @@
+
 import { useEffect, useState } from "react";
+
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  
 
   useEffect(() => {
-    fetch("/services.json")
+    fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
+   
+  
+   
+       
+    
+   
   return (
-    < >
+    <>
       <h2 className="text-4xl text-center font-bold uppercase py-8 mt-10">
-        our teams
+        our Services
         <div className="mt-2 mx-auto w-80">
           <span className="inline-block w-52 h-1 bg-blue-500 rounded-full" />
           <span className="inline-block w-10 h-1 ml-1 bg-blue-500 rounded-full" />
@@ -20,21 +29,14 @@ const Services = () => {
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services.map((service) => (
-          
-            <div key={service.name} className="card  bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <img
-                  src={service.image}
-                  alt="Shoes"
-                  className="rounded-xl h-60"
-                />
-              </figure>
-              <div className="card-body items-center text-center">
-                <h2 className="card-title">{service.name}</h2>
-                <p>Role: {service.description}</p>
-              </div>
+          <div key={service._id} className="card  bg-base-100 shadow-xl text-center">
+            <div className="card-body">
+              <h2 className="card-title block">Name:{service.name}</h2>
+              <p className="font-semibold text-blue-500">Price : {service.price}</p>
+              <p>Description : {service.description}</p>
+              
             </div>
-          
+          </div>
         ))}
       </div>
     </>
